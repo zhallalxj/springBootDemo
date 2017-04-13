d4192239899068c39bd2fd4f975725ca8704dc67
zhallalxj <zhallalxj5201314>
2017-04-13 11:36:25 +0800

    集成Redis

--- src/main/java/org/zh/Application.java
+++ src/main/java/org/zh/Application.java
@@ -3,12 +3,14 @@ package org.zh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by ZhaoHang on 2017/4/13.
  */
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableRedisHttpSession
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/org/zh/controller/TestController.java
+++ src/main/java/org/zh/controller/TestController.java
@@ -1,8 +1,10 @@
 package org.zh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zh.service.RedisService;
 
 /**
  * Created by ZhaoHang on 2017/4/13.
@@ -11,10 +13,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/")
 public class TestController {
 
+    @Autowired
+    private RedisService redisService;
+
     @RequestMapping
     @ResponseBody
     public String testHello(){
         return "Test";
+    }
+
+    @RequestMapping(value = "/get")
+    @ResponseBody
+    public String get(){
+
+        Object o = redisService.get("Test");
+        return o.toString();
+
+    }
+
+
+    @RequestMapping(value = "/set")
+    @ResponseBody
+    public String set(){
+
+        redisService.set("Test","Test");
+        return "设置成功";
+
     }
 
 }
